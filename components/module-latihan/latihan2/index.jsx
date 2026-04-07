fd34fcb6326469e279977258101a0e801e8aca4d
Didi Haryadi <didihariyadi212@gmail.com>
2026-04-07 17:33:39 +0700

    pertemuan2

---
+++ components/module-latihan/latihan2/index.jsx
@@ -0,0 +1,71 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Latihan2 = () => {
+  const personalData = {
+    name: "Anton Sukamto",
+    jurusan: "Teknologi Informasi",
+    aktif: true,
+  };
+
+  const course_lists = [
+    { id: 1, name: "Mobile Programming", code: "PPB01" },
+    { id: 2, name: "Web Programming", code: "PW04" },
+    { id: 3, name: "Calculus", code: "AC09" },
+  ];
+
+  const extracurricular = ["Basketball", "Robotics", "Mentoring"];
+  const total_point = 120;
+  const criteria_point = total_point >= 300;
+
+  const Latihan2 = () => {
+  const DataDiri = {
+    name: "Didi Haryadi",
+    jurusan: "Teknologi Informasi",
+    aktif: true,
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Personal Information</Text>
+      <Text>Fullname: {personalData.name}</Text>
+      <Text>Department: {personalData.jurusan}</Text>
+      <Text>Status: {personalData.aktif ? "Active" : "Not Active"}</Text>
+
+      <Text style={styles.title}>My Courses</Text>
+      {course_lists.map((course) => (
+        <Text key={course.id}>
+          {course.code} - {course.name}
+        </Text>
+      ))}
+
+      <Text style={styles.title}>Extracurricular</Text>
+      {extracurricular.map((item, index) => (
+        <Text key={index}>- {item}</Text>
+      ))}
+
+      <Text style={styles.title}>Evaluation Criteria</Text>
+      <Text>{criteria_point ? "Eligible" : "Ineligible"}</Text>
+
+      <Text style={styles.title}>Data Diri Mahasiswa</Text>
+      <Text>Nama: Didi Haryadi</Text>
+      <Text>NIM: 252310060</Text>
+      <Text>Jurusan: Teknologi Informasi</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    marginTop: 15,
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
+
+export default Latihan2;
